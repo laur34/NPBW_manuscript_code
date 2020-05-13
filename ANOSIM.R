e281c825e7ab38c607f4d68a609496b93c1312c9
laur34 <lv70xo@gmail.com>
2020-05-13 12:34:26 +0200

    finished draft of R script for ANOSIM test

--- ANOSIM.R
+++ ANOSIM.R
@@ -2,10 +2,10 @@
 ## 12.5.2020 LH
 ## Tutorial at https://jkzorz.github.io/2019/06/11/ANOSIM-test.html
 
-# Read in an OTU table with columns as OTUs and rows as samples.
+# Read in an OTU table with columns as OTUs and rows as samples, with metadata as (a) column(s).
 #install.packages("vegan")
 library(vegan)
-setwd("/media/laur/wdhdd11/allNPBW/")
+#setwd("/media/laur/wdhdd11/allNPBW/")
 data <- read.table("/media/laur/wdhdd1/NPBW_manuscript_code/Reordered_for_pest_R_5_newFebNPBW_VL2.tsv", header=T, sep="\t", stringsAsFactors = F)
 # Clean/prepare table to meet above conditions.
 # Replace blanks with NAs.
@@ -24,14 +24,39 @@ data <- data[, !grepl("Etoh", names(data))]
 data <- data[, !grepl("semi", names(data))]
 data <- data[, !grepl("filter", names(data))]
 head(data)
-head(data[13:ncol(data)])
-data <- data[13:ncol(data)]
+
 
 #Subset to BINs and sample data
-bindata <- data[,c(9,14:ncol(data))] #See Mantel test script...
+bindata <- data[,c(9,14:ncol(data))]
+#Combine by BIN to avoid duplicate row names (changes column 1 name to Group.1).
+bindata1 <- aggregate(bindata[,2:ncol(bindata)], by=list(bindata[,1]), FUN=sum )
 
-#Transpose
-#data1 <- data[,-1]
-#rownames(data1) <- data[,1]
-#pc <- t(data1)
-#class(pc)
+#Transpose (first get rid of the column 1 name, then make it the row names).
+bindata2 <- bindata1[,-1]
+rownames(bindata2) <- bindata1[,1]
+
+tbindata2 <- data.frame(t(bindata2))
+#write.csv(tbindata2, file="tbindata2_int_anosim.csv")
+#Add metadata into a column
+tbindatam <- cbind.data.frame(tbindata2, as.vector(c(rep("outside",60),rep("inside",120))) )
+ncol(tbindatam) #last col is md
+#Make the data frame into a matrix of abundance info, after changing row names into a column.
+tbindatam1 <- cbind(rownames(tbindatam), data.frame(tbindatam, row.names = NULL))
+colnames(tbindatam1)[which(names(tbindatam1)=="rownames(tbindatam)")] <- "SampleName"
+colnames(tbindatam1)[which(names(tbindatam1)=="as.vector.c.rep..outside...60...rep..inside...120...")] <- "Location"
+
+com <- tbindatam1[-ncol(tbindatam1)]
+com <- com[-1]
+m_com <- as.matrix(com)
+
+#Now use the function “anosim” from the package vegan to test whether there is a statistical difference between
+#groups (inside and outside of the park).
+#In the following code,  ‘m_com’ is the community abundance matrix I am testing;
+#tbindatam1$Location is the column in df that contains the grouping information for samples.
+#‘distance = “bray”’ is the dissimilarity measure (Bray-Curtis) that I would like to use in the analysis; 
+#and ‘permutations = 9999’ is the number of permutations done with the data to determine significance.
+
+ano = anosim(m_com, tbindatam1$Location, distance = "bray", permutations = 9999)
+### wait for it! ###
+ano #Reults: ANOSIM statistic: 0.2073,  Significance: 1e-04
+# Reject the null.
